1f70534e1b620985bcad03534633895d274a63b4
MD. Shohel Rana <shohel065931@gmail.com>
2025-06-12 07:05:13 +0600

    ty

--- register.php
+++ register.php
@@ -5,7 +5,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $password = $_POST['password'];
     $email = $_POST['email'];
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-    
+
+    $sql = "INSERT INTO users (username, password, email)
+            VALUES ('$username', '$email', '$password')";
+
+
+   if( conn->query($sql) === TRUE) {
+      echo "Registration Successful. <a href='login.php'>Login here</a>";
+   } else{
+     echo "Error:" . $conn->error;
+   }
+
 }
 
 
