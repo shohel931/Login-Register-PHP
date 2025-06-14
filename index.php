0c8828733f226cd1e4e831462eff1ee45e75b2aa
MD. Shohel Rana <shohel065931@gmail.com>
2025-06-14 21:49:03 +0600

    r

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
-<?php 
-echo "Hello, World! This is a simple PHP script.";
-
-
-
-
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <title>Login/Register</title>
+</head>
+<body>
+    
+</body>
+</html>
