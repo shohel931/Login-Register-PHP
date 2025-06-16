dc5a53c887cee9ca5a976077d9c63d245b393182
MD. Shohel Rana <shohel065931@gmail.com>
2025-06-16 17:37:29 +0600

    tyt

--- login.php
+++ login.php
@@ -1,26 +1,5 @@
 <?php 
-session_start();
-include 'config.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    $sql = "SELECT * FROM users WHERE username='$username'";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows === 1) {
-        $row = $result->fetch_assoc();
-        if (password_verify($password, $row['password'])) {
-            $_SESSION['user'] = $row['username'];
-            header("Location: dashboard.php");
-        } else {
-            echo "<p class='wrong_pass'>Wrong password!</p>";
-        }
-    } else {
-        echo " <p class='user_pass'>User not found!</p>";
-    }
-}
 ?>
 
 
--- register.php
+++ register.php
@@ -1,36 +1,5 @@
 <?php 
-include 'config.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-
-    if ($password === $password) {
-        echo "Passwords do not match!";
-    } else {
-        $hashed_password = password_hash($password,PASSWORD_DEFAULT);
-        $sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$hashed_password')";
-
-        if ($conn->query($sql) === TRUE) {
-            echo "Registration successful!";
-            header("Location: login.php");
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-    }
-}
-session_start();
-if (isset($_SESSION['user'])) {
-    header("Location: dashboard.php");
-    exit;
-}
-if (isset($_GET['error'])) {
-    echo "<script>alert('".$_GET['error']."');</script>";
-}
-if (isset($_GET['success'])) {
-    echo "<script>alert('".$_GET['success']."');</script>";
-}
 
 ?>
 
