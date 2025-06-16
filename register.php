cc84580ba044b44964691f24097352694bc2f1d7
MD. Shohel Rana <shohel065931@gmail.com>
2025-06-16 18:35:22 +0600

    ff

--- register.php
+++ register.php
@@ -5,6 +5,14 @@ if (isset($_POST['submit'])) {
     $username = $_POST['username'];
     $email = $_POST['email'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+
+    $sql = "INSERT TNTO users (username, email, password) VALUES ('$username', '$email', '$password')";
+
+    
+
+
+
 }
 
 ?>
