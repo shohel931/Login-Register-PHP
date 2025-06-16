4f0ab621785a6a031eb1033fd67896e1b9f5e1aa
MD. Shohel Rana <shohel065931@gmail.com>
2025-06-16 18:59:47 +0600

    gg

--- dashboard.php
+++ dashboard.php
@@ -9,6 +9,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css">
+    <link rel="shortcut icon" href="./img/favicon.jpg" type="image/x-icon">
     <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     <title>Dashboard</title>
 </head>
